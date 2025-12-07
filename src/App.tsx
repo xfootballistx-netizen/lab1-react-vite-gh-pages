9ce5f183648a3cae93ebe5620102d512baac6b23
xfootballistx-netizen <xfootballistx@gmail.com>
2025-12-07 20:18:22 +0300

    add tests

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Counter from './components/Counter'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <Counter />
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
